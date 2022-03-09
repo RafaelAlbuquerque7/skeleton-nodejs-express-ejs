bcc57c1b74ef5fefafa7decc47b0b315cccd8819
Rafael Albuquerque <86443996+RafaelAlbuquerque7@users.noreply.github.com>
2022-03-09 20:56:28 +0000

    Criamos o CRUD - Create e adicionamos 3 autores no banco de dados.

--- models/autor.js
+++ models/autor.js
@@ -5,11 +5,15 @@ class Autor {
    const connect = await db.connect();
     return await connect.query("select * from autores")
  }
- static inserir(){
-   
+ static async inserir(data){
+    const connect = await db.connect();
+   const sql = "insert into autores(nome, sobrenome, data_de_nascimento) values($1,$2,$3)";
+   const values = [data.nome, data.sobrenome, data.data_de_nascimento];
+   return await connect.query(slq, values);
  }
   static atualizar(){
-   
+
+  
  }
   static deletar(){
    
