88d09df883654c7fca34c694637de15f8806468e
Rafael Albuquerque <86443996+RafaelAlbuquerque7@users.noreply.github.com>
2022-03-23 21:23:10 +0000

    Adicionamos  ao "/deletar" o metodo "delete" e deletei 1

--- routes/index.js
+++ routes/index.js
@@ -15,12 +15,11 @@ router.post('/inserir', async function(req, res, next){
 
 });
 
-router.get('/deletar', async function(req, res, next) {
-  const deleta = {
-    id: "2" 
-  }
-     const autores = await Autor.deletar(deleta);
+router.delete('/deletar', async function(req, res, next) {
+const autores = await Autor.deletar(req.body);
   res.json(autores)
+
+  
 });
 
 router.put('/atualizar', async function(req, res, next) {
